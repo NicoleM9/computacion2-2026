87f2201662ec2aaab7c383a26475fe4c4e3cc235
NicoleM9 <nicolemartinez9999@gmail.com>
2026-08-09 16:02:26 -0300

    Ejercicio 6: SharedMemory y ShareableList

--- bloque_7/shareable_list.py
+++ bloque_7/shareable_list.py
@@ -1,1 +1,64 @@
+#!/usr/bin/env python3
 
+from multiprocessing import Process
+from multiprocessing import shared_memory
+
+
+def actualizar_datos(nombre):
+
+    lista = shared_memory.ShareableList(
+        name=nombre
+    )
+
+    lista[0] = 42
+    lista[1] = 3.14159
+    lista[2] = "actualizado"
+    lista[3] = False
+
+    print(
+        "[WORKER] Lista actualizada:",
+        list(lista)
+    )
+
+    lista.shm.close()
+
+
+lista = shared_memory.ShareableList(
+    [
+        0,
+        0.0,
+        "                    ",
+        True
+    ],
+    name="mi_lista_comp"
+)
+
+print("Antes:", list(lista))
+
+p = Process(
+    target=actualizar_datos,
+    args=(lista.shm.name,)
+)
+
+p.start()
+p.join()
+
+print("Después:", list(lista))
+
+# Verificación
+esperado = [
+    42,
+    3.14159,
+    "actualizado",
+    False
+]
+
+print("\n=== VERIFICACION ===")
+
+if list(lista) == esperado:
+    print("Datos correctos.")
+else:
+    print("ERROR en los datos.")
+
+lista.shm.close()
+lista.shm.unlink()
--- bloque_7/shared_memory.py
+++ bloque_7/shared_memory.py
@@ -1,1 +1,101 @@
+#!/usr/bin/env python3
 
+from multiprocessing import Process, shared_memory
+import struct
+import time
+
+
+NUM = 10
+TAMAÑO = NUM * 4 + 1
+
+
+def productor(nombre, cantidad):
+
+    shm = shared_memory.SharedMemory(
+        name=nombre
+    )
+
+    print("[PRODUCTOR] Escribiendo valores...")
+
+    for i in range(cantidad):
+
+        struct.pack_into(
+            "i",
+            shm.buf,
+            i * 4,
+            i * i
+        )
+
+    # Indicar que terminó
+    shm.buf[-1] = 1
+
+    print("[PRODUCTOR] Datos escritos")
+
+    shm.close()
+
+
+def consumidor(nombre, cantidad):
+
+    shm = shared_memory.SharedMemory(
+        name=nombre
+    )
+
+    print("[CONSUMIDOR] Esperando datos...")
+
+    while shm.buf[-1] != 1:
+        time.sleep(0.01)
+
+    valores = []
+
+    for i in range(cantidad):
+
+        valor = struct.unpack_from(
+            "i",
+            shm.buf,
+            i * 4
+        )[0]
+
+        valores.append(valor)
+
+    print("[CONSUMIDOR] Valores:", valores)
+
+    esperados = [
+        i * i
+        for i in range(cantidad)
+    ]
+
+    if valores == esperados:
+        print("[CONSUMIDOR] VERIFICACION CORRECTA")
+    else:
+        print("[CONSUMIDOR] ERROR")
+
+    shm.close()
+
+
+shm = shared_memory.SharedMemory(
+    create=True,
+    size=TAMAÑO
+)
+
+print("Memoria creada:", shm.name)
+
+productor_proceso = Process(
+    target=productor,
+    args=(shm.name, NUM)
+)
+
+consumidor_proceso = Process(
+    target=consumidor,
+    args=(shm.name, NUM)
+)
+
+consumidor_proceso.start()
+productor_proceso.start()
+
+productor_proceso.join()
+consumidor_proceso.join()
+
+shm.close()
+shm.unlink()
+
+print("Memoria compartida eliminada.")
