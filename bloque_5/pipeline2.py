f973e222062cb7c77d94570a88a7e4f01b7f46fd
NicoleM9 <nicolemartinez9999@gmail.com>
2026-08-09 09:57:37 -0300

    Ejercicio 4: pipeline de comandos

--- bloque_5/pipeline2.py
+++ bloque_5/pipeline2.py
@@ -1,1 +1,39 @@
+#!/usr/bin/env python3
 
+import os
+
+
+# Crear el pipe
+read_fd, write_fd = os.pipe()
+
+
+# Crear el primer hijo: ls -l
+pid1 = os.fork()
+
+if pid1 == 0:
+    # stdout de ls -> pipe
+    os.close(read_fd)
+    os.dup2(write_fd, 1)
+    os.close(write_fd)
+
+    os.execvp("ls", ["ls", "-l"])
+
+
+# Crear el segundo hijo: grep .py
+pid2 = os.fork()
+
+if pid2 == 0:
+    # stdin de grep <- pipe
+    os.close(write_fd)
+    os.dup2(read_fd, 0)
+    os.close(read_fd)
+
+    os.execvp("grep", ["grep", ".py"])
+
+
+# Padre
+os.close(read_fd)
+os.close(write_fd)
+
+os.waitpid(pid1, 0)
+os.waitpid(pid2, 0)
--- bloque_5/pipeline3.py
+++ bloque_5/pipeline3.py
@@ -1,1 +1,82 @@
+#!/usr/bin/env python3
 
+import os
+
+
+# Pipe 1: cat -> cut
+read1, write1 = os.pipe()
+
+# Pipe 2: cut -> sort
+read2, write2 = os.pipe()
+
+
+# =================================
+# PRIMER HIJO: cat /etc/passwd
+# =================================
+
+pid1 = os.fork()
+
+if pid1 == 0:
+    os.close(read1)
+    os.close(read2)
+    os.close(write2)
+
+    # stdout de cat -> pipe 1
+    os.dup2(write1, 1)
+    os.close(write1)
+
+    os.execvp("cat", ["cat", "/etc/passwd"])
+
+
+# =================================
+# SEGUNDO HIJO: cut -d: -f1
+# =================================
+
+pid2 = os.fork()
+
+if pid2 == 0:
+    os.close(write1)
+    os.close(read2)
+
+    # stdin de cut <- pipe 1
+    os.dup2(read1, 0)
+    os.close(read1)
+
+    # stdout de cut -> pipe 2
+    os.dup2(write2, 1)
+    os.close(write2)
+
+    os.execvp("cut", ["cut", "-d:", "-f1"])
+
+
+# =================================
+# TERCER HIJO: sort
+# =================================
+
+pid3 = os.fork()
+
+if pid3 == 0:
+    os.close(read1)
+    os.close(write1)
+    os.close(write2)
+
+    # stdin de sort <- pipe 2
+    os.dup2(read2, 0)
+    os.close(read2)
+
+    os.execvp("sort", ["sort"])
+
+
+# =================================
+# PADRE
+# =================================
+
+os.close(read1)
+os.close(write1)
+os.close(read2)
+os.close(write2)
+
+# Esperar a los tres hijos
+os.waitpid(pid1, 0)
+os.waitpid(pid2, 0)
+os.waitpid(pid3, 0)
