9993c9845f426ad09a7fba205646be315651ef4e
NicoleM9 <nicolemartinez9999@gmail.com>
2026-08-09 09:51:40 -0300

    Ejercicio 2:redireccion basica

--- bloque_5/redireccion.py
+++ bloque_5/redireccion.py
@@ -1,1 +1,36 @@
+#!/usr/bin/env python3
 
+import os
+import sys
+
+
+print("Este mensaje va a la terminal")
+
+# Guardar stdout original
+stdout_original = os.dup(1)
+
+# Abrir archivo destino
+archivo = os.open(
+    "/tmp/salida.txt",
+    os.O_CREAT | os.O_WRONLY | os.O_TRUNC,
+    0o644
+)
+
+# Redirigir stdout al archivo
+os.dup2(archivo, 1)
+
+# Ya no necesitamos este descriptor
+os.close(archivo)
+
+# Estos mensajes van al archivo
+print("Este mensaje va al archivo")
+print("Y este también")
+
+sys.stdout.flush()
+
+# Restaurar stdout original
+os.dup2(stdout_original, 1)
+os.close(stdout_original)
+
+print("Volvimos a la terminal")
+print("Revisá el contenido de /tmp/salida.txt")
--- bloque_5/separar_salidas.py
+++ bloque_5/separar_salidas.py
@@ -1,1 +1,16 @@
+#!/usr/bin/env python3
 
+import sys
+import os
+
+
+# stdout
+print("Mensaje normal a stdout")
+sys.stdout.write("Otro mensaje a stdout\n")
+os.write(1, b"Y otro mas directo al fd 1\n")
+
+
+# stderr
+print("Mensaje de error a stderr", file=sys.stderr)
+sys.stderr.write("Otro error a stderr\n")
+os.write(2, b"Error directo al fd 2\n")
