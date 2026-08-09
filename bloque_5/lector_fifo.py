66829a484882fd1c4dca3d1705af036ff40a0e07
NicoleM9 <nicolemartinez9999@gmail.com>
2026-08-09 10:05:36 -0300

    Ejercicio 7:named pipe para comunicacion entre programas

--- bloque_5/lector_fifo.py
+++ bloque_5/lector_fifo.py
@@ -1,1 +1,16 @@
+#!/usr/bin/env python3
 
+FIFO = "/tmp/mi_canal"
+
+
+print(f"Leyendo de {FIFO}...")
+
+
+with open(FIFO, "r") as f:
+
+    for linea in f:
+
+        print(f"Recibido: {linea.strip()}")
+
+
+print("Lectura completada (el escritor cerró el pipe)")
